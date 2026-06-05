aadbe35bec48957faa6e65fe7962d497a65ba0ca
Alejandro González Luzuriaga <116267260+alejandroluzuriaga@users.noreply.github.com>
2026-06-05 09:49:07 +0200

    Add: longest-substring-without-duplicates - submission-10

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-10.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0, l = 0, r = 1;
+        List<Character> word = new LinkedList<>();
+        if (s == null || s.length() == 0)
+            return 0;
+        if (s.length() == 1)
+            return 1;
+
+        word.add(s.charAt(0));
+        while(l<r && l < s.length() && r < s.length()){
+            if (r < s.length()){
+                max = Math.max(max, word.size());
+                if (s.charAt(l) == s.charAt(r)){
+                    l++;
+                    r++;
+                    continue;
+                }
+                if (!word.contains(s.charAt(r))){
+                    word.add(s.charAt(r));
+                    r++;
+                }
+                else{
+                    l++;
+                    word.clear();
+                    word.add(s.charAt(l));
+                    r++;
+                }
+                max = Math.max(max, word.size());
+            }
+        }
+        return max;
+    }
+}
