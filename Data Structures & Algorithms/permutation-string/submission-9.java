8be310ff5d9702c38d61a73e2c86d1a1c2dd825a
Alejandro González Luzuriaga <116267260+alejandroluzuriaga@users.noreply.github.com>
2026-06-05 19:10:11 +0200

    Add: permutation-string - submission-9

---
+++ Data Structures & Algorithms/permutation-string/submission-9.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        Map<Character, Integer> freqs = new TreeMap<>();
+        Map<Character, Integer> freqsWindow = new TreeMap<>();
+        for(int i = 0; i < s1.length(); i++)
+            freqs.put(s1.charAt(i), freqs.get(s1.charAt(i)) != null ? freqs.get(s1.charAt(i))+1 : 1);
+
+        int l = 0;
+        int r = 0;
+        int valuesToFind = freqs.values().size();
+        while(l < s2.length() && r < s2.length()){
+            int windowLength = (r-l)+1;
+            if (!freqs.containsKey(s2.charAt(r))){
+                if (freqsWindow.containsKey(s2.charAt(l)))
+                    if (freqsWindow.get(s2.charAt(l)) == 0)
+                        freqsWindow.remove(s2.charAt(l));
+                    else
+                        freqsWindow.put(s2.charAt(l), freqsWindow.get(s2.charAt(l))-1);
+
+                if (freqsWindow.containsKey(s2.charAt(r)))
+                    if (freqsWindow.get(s2.charAt(r)) == 0)
+                        freqsWindow.remove(s2.charAt(r));
+                    else
+                        freqsWindow.put(s2.charAt(r), freqsWindow.get(s2.charAt(r)) != null ? freqsWindow.get(s2.charAt(r))+1 : 1);
+                l++;
+                r++;
+                continue;
+            }else{
+                freqsWindow.put(s2.charAt(r), freqsWindow.get(s2.charAt(r)) != null ? freqsWindow.get(s2.charAt(r))+1 : 1);
+                if (windowLength == s1.length()){
+                    if (freqs.equals(freqsWindow))
+                        return true;
+                    freqsWindow.clear();
+                    l++;
+                    r = l;
+                }
+                else
+                    r++;
+            }
+        }
+        return false;
+    }
+}
