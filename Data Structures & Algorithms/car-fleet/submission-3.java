6d4b7aa1491b42098acddc164be6b092889043c9
Alejandro González Luzuriaga <116267260+alejandroluzuriaga@users.noreply.github.com>
2026-06-07 18:38:29 +0200

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        Set<Pair<Integer, Double>> s = new TreeSet<>(new Comparator<Pair<Integer, Double>>(){ 
+            public int compare(Pair<Integer, Double> p1, Pair<Integer, Double> p2){
+                if (p1.getKey() < p2.getKey())
+                    return 1;
+                else
+                    return -1;
+            }});
+        int fleets = 0;
+        for(int i = 0; i < position.length; i++){
+            Double reachTime = (target - position[i]) / (double) speed[i];
+            s.add(new Pair<Integer, Double>(position[i], reachTime));
+        }
+        Double currentFleetReachTime = 0.0;
+        for(Pair<Integer, Double> elem : s){
+            Double reachTime = elem.getValue();
+            if (reachTime > currentFleetReachTime){
+                currentFleetReachTime = reachTime;
+                fleets++;
+            }
+            else
+                continue;
+        }
+        return fleets;
+    }
+}
